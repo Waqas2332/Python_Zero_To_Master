8d714dd811341a83b0bce2f6ba2940a13a71a7b0
Waqas2332 <wmunir232@gmail.com>
2023-04-28 21:49:18 +0500

    Done with basic numbers methods

---
+++ Python Lessons/basic_numbers.py
@@ -0,0 +1,10 @@
+#abs(variable) ==> returns the absolute value of passed variable
+num = -5
+print(abs(num))
+#round(varible,percision count) ==> rounds the passed value to the nearly number w.r.t given percision
+print(round(3.776,0))
+
+#Comparison Operator
+"""
+== ,>= , <= , > , < ,!=
+"""
