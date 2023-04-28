0f6e6b9ad259a6f3e58a1276c80cdb664a118bcf
Waqas2332 <wmunir232@gmail.com>
2023-04-28 21:43:59 +0500

    Done with operators,datatypes,type conversions

---
+++ Python Lessons/data_types_operations.py
@@ -0,0 +1,22 @@
+#integers ==> Numbers without decimal points
+12343
+
+#string ==> Array of characters
+"Waqas"
+
+#float ==> Numbers with decimal points
+123.32
+
+#Boolean ==> True OR False
+
+
+#type(variable) ==> function used to compute the datatype of any variable
+name="Waqas"
+print(type(name))
+
+#Type Conversion
+#str() ==> used to convert any datatype in string
+#float() ==> used to convert any datatype in float
+#int ==> used to convert any datatype in integer
+
+#operations ==> + , - , * , / , % , **
